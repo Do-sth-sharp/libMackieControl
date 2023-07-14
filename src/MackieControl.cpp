a675107cf62d1e1535da32714eaf4a8d30edb503
無常 <3142324836@qq.com>
2023-07-15 01:04:52 +0800

    Fixed varibles in headers

--- src/MackieControl.cpp
+++ src/MackieControl.cpp
@@ -5,13 +5,135 @@
  * \author	WuChang
  * \email	31423836@qq.com
  * \date	July 2023
- * \version	1.0.0
+ * \version	1.0.1
  * \license	MIT License
  *********************************************************************/
 
 #include "MackieControl.h"
 
 namespace mackieControl {
+	constinit auto validSysExMessage = std::to_array({
+		SysExMessage::DeviceQuery,
+		SysExMessage::HostConnectionQuery,
+		SysExMessage::HostConnectionReply,
+		SysExMessage::HostConnectionConfirmation,
+		SysExMessage::HostConnectionError,
+		SysExMessage::LCDBackLightSaver,
+		SysExMessage::TouchlessMovableFaders,
+		SysExMessage::FaderTouchSensitivity,
+		SysExMessage::GoOffline,
+		SysExMessage::TimeCodeBBTDisplay,
+		SysExMessage::Assignment7SegmentDisplay,
+		SysExMessage::LCD,
+		SysExMessage::VersionRequest,
+		SysExMessage::VersionReply,
+		SysExMessage::ChannelMeterMode,
+		SysExMessage::GlobalLCDMeterMode,
+		SysExMessage::AllFaderstoMinimum,
+		SysExMessage::AllLEDsOff,
+		SysExMessage::Reset
+		});
+	constexpr bool isValidSysExMessage(SysExMessage mes) {
+		return std::find(validSysExMessage.begin(), validSysExMessage.end(), mes) != validSysExMessage.end();
+	}
+	constexpr bool isValidSysExMessage(int mes) {
+		return isValidSysExMessage(static_cast<SysExMessage>(mes));
+	}
+
+	constinit auto validVelocityMessage = std::to_array({
+		VelocityMessage::Off,
+		VelocityMessage::Flashing,
+		VelocityMessage::On
+		});
+	constexpr bool isValidVelocityMessage(VelocityMessage mes) {
+		return std::find(validVelocityMessage.begin(), validVelocityMessage.end(), mes) != validVelocityMessage.end();
+	}
+	constexpr bool isValidVelocityMessage(int mes) {
+		return isValidVelocityMessage(static_cast<VelocityMessage>(mes));
+	}
+
+	constinit auto validNoteMessage = std::to_array({
+		NoteMessage::RECRDYCh1, NoteMessage::RECRDYCh2, NoteMessage::RECRDYCh3, NoteMessage::RECRDYCh4,
+		NoteMessage::RECRDYCh5, NoteMessage::RECRDYCh6, NoteMessage::RECRDYCh7, NoteMessage::RECRDYCh8,
+		NoteMessage::SOLOCh1, NoteMessage::SOLOCh2, NoteMessage::SOLOCh3, NoteMessage::SOLOCh4,
+		NoteMessage::SOLOCh5, NoteMessage::SOLOCh6, NoteMessage::SOLOCh7, NoteMessage::SOLOCh8,
+		NoteMessage::MUTECh1, NoteMessage::MUTECh2, NoteMessage::MUTECh3, NoteMessage::MUTECh4,
+		NoteMessage::MUTECh5, NoteMessage::MUTECh6, NoteMessage::MUTECh7, NoteMessage::MUTECh8,
+		NoteMessage::SELECTCh1, NoteMessage::SELECTCh2, NoteMessage::SELECTCh3, NoteMessage::SELECTCh4,
+		NoteMessage::SELECTCh5, NoteMessage::SELECTCh6, NoteMessage::SELECTCh7, NoteMessage::SELECTCh8,
+		NoteMessage::VSelectCh1, NoteMessage::VSelectCh2, NoteMessage::VSelectCh3, NoteMessage::VSelectCh4,
+		NoteMessage::VSelectCh5, NoteMessage::VSelectCh6, NoteMessage::VSelectCh7, NoteMessage::VSelectCh8,
+		NoteMessage::ASSIGNMENTTRACK, NoteMessage::ASSIGNMENTSEND, NoteMessage::ASSIGNMENTPANSURROUND,
+		NoteMessage::ASSIGNMENTPLUGIN, NoteMessage::ASSIGNMENTEQ, NoteMessage::ASSIGNMENTINSTRUMENT,
+		NoteMessage::FADERBANKSBANKLeft, NoteMessage::FADERBANKSBANKRight,
+		NoteMessage::FADERBANKSCHANNELLeft, NoteMessage::FADERBANKSCHANNELRight,
+		NoteMessage::FLIP,
+		NoteMessage::GLOBALVIEW,
+		NoteMessage::NAMEVALUE,
+		NoteMessage::SMPTEBEATS,
+		NoteMessage::Function1, NoteMessage::Function2, NoteMessage::Function3, NoteMessage::Function4,
+		NoteMessage::Function5, NoteMessage::Function6, NoteMessage::Function7, NoteMessage::Function8,
+		NoteMessage::GLOBALVIEWMIDITRACKS, NoteMessage::GLOBALVIEWINPUTS,
+		NoteMessage::GLOBALVIEWAUDIOTRACKS, NoteMessage::GLOBALVIEWAUDIOINSTRUMENT,
+		NoteMessage::GLOBALVIEWAUX, NoteMessage::GLOBALVIEWBUSSES,
+		NoteMessage::GLOBALVIEWOUTPUTS, NoteMessage::GLOBALVIEWUSER,
+		NoteMessage::SHIFT, NoteMessage::OPTION, NoteMessage::CONTROL, NoteMessage::CMDALT,
+		NoteMessage::AUTOMATIONREADOFF, NoteMessage::AUTOMATIONWRITE, NoteMessage::AUTOMATIONTRIM,
+		NoteMessage::AUTOMATIONTOUCH, NoteMessage::AUTOMATIONLATCH,
+		NoteMessage::GROUP,
+		NoteMessage::UTILITIESSAVE, NoteMessage::UTILITIESUNDO,
+		NoteMessage::UTILITIESCANCEL, NoteMessage::UTILITIESENTER,
+		NoteMessage::MARKER,
+		NoteMessage::NUDGE,
+		NoteMessage::CYCLE,
+		NoteMessage::DROP,
+		NoteMessage::REPLACE,
+		NoteMessage::CLICK,
+		NoteMessage::SOLO,
+		NoteMessage::REWIND, NoteMessage::FASTFWD, NoteMessage::STOP, NoteMessage::PLAY, NoteMessage::RECORD,
+		NoteMessage::CursorUp, NoteMessage::CursorDown, NoteMessage::CursorLeft, NoteMessage::CursorRight,
+		NoteMessage::Zoom,
+		NoteMessage::Scrub,
+		NoteMessage::UserSwitchA, NoteMessage::UserSwitchB,
+		NoteMessage::FaderTouchCh1, NoteMessage::FaderTouchCh2,
+		NoteMessage::FaderTouchCh3, NoteMessage::FaderTouchCh4,
+		NoteMessage::FaderTouchCh5, NoteMessage::FaderTouchCh6,
+		NoteMessage::FaderTouchCh7, NoteMessage::FaderTouchCh8,
+		NoteMessage::FaderTouchMaster,
+		NoteMessage::SMPTELED,
+		NoteMessage::BEATSLED,
+		NoteMessage::RUDESOLOLIGHT,
+		NoteMessage::Relayclick
+		});
+	constexpr bool isValidNoteMessage(NoteMessage mes) {
+		return std::find(validNoteMessage.begin(), validNoteMessage.end(), mes) != validNoteMessage.end();
+	}
+	constexpr bool isValidNoteMessage(int mes) {
+		return isValidNoteMessage(static_cast<NoteMessage>(mes));
+	}
+
+	constinit auto validCCMessage = std::to_array({
+		CCMessage::VPot1, CCMessage::VPot2, CCMessage::VPot3, CCMessage::VPot4,
+		CCMessage::VPot5, CCMessage::VPot6, CCMessage::VPot7, CCMessage::VPot8,
+		CCMessage::ExternalController,
+		CCMessage::VPotLEDRing1, CCMessage::VPotLEDRing2, CCMessage::VPotLEDRing3, CCMessage::VPotLEDRing4,
+		CCMessage::VPotLEDRing5, CCMessage::VPotLEDRing6, CCMessage::VPotLEDRing7, CCMessage::VPotLEDRing8,
+		CCMessage::JogWheel,
+		CCMessage::TimeCodeBBTDisplay1, CCMessage::TimeCodeBBTDisplay2,
+		CCMessage::TimeCodeBBTDisplay3, CCMessage::TimeCodeBBTDisplay4,
+		CCMessage::TimeCodeBBTDisplay5, CCMessage::TimeCodeBBTDisplay6,
+		CCMessage::TimeCodeBBTDisplay7, CCMessage::TimeCodeBBTDisplay8,
+		CCMessage::TimeCodeBBTDisplay9, CCMessage::TimeCodeBBTDisplay10,
+		CCMessage::Assignment7SegmentDisplay1, CCMessage::Assignment7SegmentDisplay2,
+		CCMessage::Assignment7SegmentDisplay3
+		});
+	constexpr bool isValidCCMessage(CCMessage mes) {
+		return std::find(validCCMessage.begin(), validCCMessage.end(), mes) != validCCMessage.end();
+	}
+	constexpr bool isValidCCMessage(int mes) {
+		return isValidCCMessage(static_cast<CCMessage>(mes));
+	}
+
 	Message::Message(const juce::MidiMessage& midiMessage)
 		: message(midiMessage) {}
 
--- src/MackieControl.h
+++ src/MackieControl.h
@@ -5,7 +5,7 @@
  * \author	WuChang
  * \email	31423836@qq.com
  * \date	July 2023
- * \version	1.0.0
+ * \version	1.0.1
  * \license	MIT License
  *********************************************************************/
 
@@ -39,39 +39,13 @@ namespace mackieControl {
 		Reset
 	};
 	/**
-	 * Valid Mackie Control messages via MIDI system exclusive message.
+	 * Check if the message is valid.
 	 */
-	constinit auto validSysExMessage = std::to_array({
-		SysExMessage::DeviceQuery,
-		SysExMessage::HostConnectionQuery,
-		SysExMessage::HostConnectionReply,
-		SysExMessage::HostConnectionConfirmation,
-		SysExMessage::HostConnectionError,
-		SysExMessage::LCDBackLightSaver,
-		SysExMessage::TouchlessMovableFaders,
-		SysExMessage::FaderTouchSensitivity,
-		SysExMessage::GoOffline,
-		SysExMessage::TimeCodeBBTDisplay,
-		SysExMessage::Assignment7SegmentDisplay,
-		SysExMessage::LCD,
-		SysExMessage::VersionRequest,
-		SysExMessage::VersionReply,
-		SysExMessage::ChannelMeterMode,
-		SysExMessage::GlobalLCDMeterMode,
-		SysExMessage::AllFaderstoMinimum,
-		SysExMessage::AllLEDsOff,
-		SysExMessage::Reset
-		});
+	constexpr bool isValidSysExMessage(SysExMessage mes);
 	/**
 	 * Check if the message is valid.
 	 */
-	constexpr bool isValidSysExMessage(SysExMessage mes) { 
-		return std::find(validSysExMessage.begin(), validSysExMessage.end(), mes) != validSysExMessage.end(); }
-	/**
-	 * Check if the message is valid.
-	 */
-	constexpr bool isValidSysExMessage(int mes) {
-		return isValidSysExMessage(static_cast<SysExMessage>(mes)); }
+	constexpr bool isValidSysExMessage(int mes);
 
 	/**
 	 * Mackie Control messages via MIDI note message velocity data.
@@ -82,23 +56,13 @@ namespace mackieControl {
 		On = 127
 	};
 	/**
-	 * Valid Mackie Control messages via MIDI note message velocity data.
+	 * Check if the message is valid.
 	 */
-	constinit auto validVelocityMessage = std::to_array({
-		VelocityMessage::Off,
-		VelocityMessage::Flashing,
-		VelocityMessage::On
-		});
+	constexpr bool isValidVelocityMessage(VelocityMessage mes);
 	/**
 	 * Check if the message is valid.
 	 */
-	constexpr bool isValidVelocityMessage(VelocityMessage mes) {
-		return std::find(validVelocityMessage.begin(), validVelocityMessage.end(), mes) != validVelocityMessage.end(); }
-	/**
-	 * Check if the message is valid.
-	 */
-	constexpr bool isValidVelocityMessage(int mes) {
-		return isValidVelocityMessage(static_cast<VelocityMessage>(mes)); }
+	constexpr bool isValidVelocityMessage(int mes);
 
 	/**
 	 * Mackie Control messages via MIDI note message note number data.
@@ -143,71 +107,13 @@ namespace mackieControl {
 		Relayclick
 	};
 	/**
-	 * Valid Mackie Control messages via MIDI note message note number data.
+	 * Check if the message is valid.
 	 */
-	constinit auto validNoteMessage = std::to_array({
-		NoteMessage::RECRDYCh1, NoteMessage::RECRDYCh2, NoteMessage::RECRDYCh3, NoteMessage::RECRDYCh4,
-		NoteMessage::RECRDYCh5, NoteMessage::RECRDYCh6, NoteMessage::RECRDYCh7, NoteMessage::RECRDYCh8,
-		NoteMessage::SOLOCh1, NoteMessage::SOLOCh2, NoteMessage::SOLOCh3, NoteMessage::SOLOCh4,
-		NoteMessage::SOLOCh5, NoteMessage::SOLOCh6, NoteMessage::SOLOCh7, NoteMessage::SOLOCh8,
-		NoteMessage::MUTECh1, NoteMessage::MUTECh2, NoteMessage::MUTECh3, NoteMessage::MUTECh4,
-		NoteMessage::MUTECh5, NoteMessage::MUTECh6, NoteMessage::MUTECh7, NoteMessage::MUTECh8,
-		NoteMessage::SELECTCh1, NoteMessage::SELECTCh2, NoteMessage::SELECTCh3, NoteMessage::SELECTCh4,
-		NoteMessage::SELECTCh5, NoteMessage::SELECTCh6, NoteMessage::SELECTCh7, NoteMessage::SELECTCh8,
-		NoteMessage::VSelectCh1, NoteMessage::VSelectCh2, NoteMessage::VSelectCh3, NoteMessage::VSelectCh4,
-		NoteMessage::VSelectCh5, NoteMessage::VSelectCh6, NoteMessage::VSelectCh7, NoteMessage::VSelectCh8,
-		NoteMessage::ASSIGNMENTTRACK, NoteMessage::ASSIGNMENTSEND, NoteMessage::ASSIGNMENTPANSURROUND,
-		NoteMessage::ASSIGNMENTPLUGIN, NoteMessage::ASSIGNMENTEQ, NoteMessage::ASSIGNMENTINSTRUMENT,
-		NoteMessage::FADERBANKSBANKLeft, NoteMessage::FADERBANKSBANKRight,
-		NoteMessage::FADERBANKSCHANNELLeft, NoteMessage::FADERBANKSCHANNELRight,
-		NoteMessage::FLIP,
-		NoteMessage::GLOBALVIEW,
-		NoteMessage::NAMEVALUE,
-		NoteMessage::SMPTEBEATS,
-		NoteMessage::Function1, NoteMessage::Function2, NoteMessage::Function3, NoteMessage::Function4,
-		NoteMessage::Function5, NoteMessage::Function6, NoteMessage::Function7, NoteMessage::Function8,
-		NoteMessage::GLOBALVIEWMIDITRACKS, NoteMessage::GLOBALVIEWINPUTS,
-		NoteMessage::GLOBALVIEWAUDIOTRACKS, NoteMessage::GLOBALVIEWAUDIOINSTRUMENT,
-		NoteMessage::GLOBALVIEWAUX, NoteMessage::GLOBALVIEWBUSSES,
-		NoteMessage::GLOBALVIEWOUTPUTS, NoteMessage::GLOBALVIEWUSER,
-		NoteMessage::SHIFT, NoteMessage::OPTION, NoteMessage::CONTROL, NoteMessage::CMDALT,
-		NoteMessage::AUTOMATIONREADOFF, NoteMessage::AUTOMATIONWRITE, NoteMessage::AUTOMATIONTRIM,
-		NoteMessage::AUTOMATIONTOUCH, NoteMessage::AUTOMATIONLATCH,
-		NoteMessage::GROUP,
-		NoteMessage::UTILITIESSAVE, NoteMessage::UTILITIESUNDO,
-		NoteMessage::UTILITIESCANCEL, NoteMessage::UTILITIESENTER,
-		NoteMessage::MARKER,
-		NoteMessage::NUDGE,
-		NoteMessage::CYCLE,
-		NoteMessage::DROP,
-		NoteMessage::REPLACE,
-		NoteMessage::CLICK,
-		NoteMessage::SOLO,
-		NoteMessage::REWIND, NoteMessage::FASTFWD, NoteMessage::STOP, NoteMessage::PLAY, NoteMessage::RECORD,
-		NoteMessage::CursorUp, NoteMessage::CursorDown, NoteMessage::CursorLeft, NoteMessage::CursorRight,
-		NoteMessage::Zoom,
-		NoteMessage::Scrub,
-		NoteMessage::UserSwitchA, NoteMessage::UserSwitchB,
-		NoteMessage::FaderTouchCh1, NoteMessage::FaderTouchCh2,
-		NoteMessage::FaderTouchCh3, NoteMessage::FaderTouchCh4,
-		NoteMessage::FaderTouchCh5, NoteMessage::FaderTouchCh6,
-		NoteMessage::FaderTouchCh7, NoteMessage::FaderTouchCh8,
-		NoteMessage::FaderTouchMaster,
-		NoteMessage::SMPTELED,
-		NoteMessage::BEATSLED,
-		NoteMessage::RUDESOLOLIGHT,
-		NoteMessage::Relayclick
-		});
+	constexpr bool isValidNoteMessage(NoteMessage mes);
 	/**
 	 * Check if the message is valid.
 	 */
-	constexpr bool isValidNoteMessage(NoteMessage mes) {
-		return std::find(validNoteMessage.begin(), validNoteMessage.end(), mes) != validNoteMessage.end(); }
-	/**
-	 * Check if the message is valid.
-	 */
-	constexpr bool isValidNoteMessage(int mes) {
-		return isValidNoteMessage(static_cast<NoteMessage>(mes)); }
+	constexpr bool isValidNoteMessage(int mes);
 
 	/**
 	 * Mackie Control messages via MIDI controller message controller number data.
@@ -224,33 +130,13 @@ namespace mackieControl {
 		Assignment7SegmentDisplay1, Assignment7SegmentDisplay2, Assignment7SegmentDisplay3
 	};
 	/**
-	 * Valid Mackie Control messages via MIDI controller message controller number data.
+	 * Check if the message is valid.
 	 */
-	constinit auto validCCMessage = std::to_array({
-		CCMessage::VPot1, CCMessage::VPot2, CCMessage::VPot3, CCMessage::VPot4,
-		CCMessage::VPot5, CCMessage::VPot6, CCMessage::VPot7, CCMessage::VPot8,
-		CCMessage::ExternalController,
-		CCMessage::VPotLEDRing1, CCMessage::VPotLEDRing2, CCMessage::VPotLEDRing3, CCMessage::VPotLEDRing4,
-		CCMessage::VPotLEDRing5, CCMessage::VPotLEDRing6, CCMessage::VPotLEDRing7, CCMessage::VPotLEDRing8,
-		CCMessage::JogWheel,
-		CCMessage::TimeCodeBBTDisplay1, CCMessage::TimeCodeBBTDisplay2,
-		CCMessage::TimeCodeBBTDisplay3, CCMessage::TimeCodeBBTDisplay4,
-		CCMessage::TimeCodeBBTDisplay5, CCMessage::TimeCodeBBTDisplay6,
-		CCMessage::TimeCodeBBTDisplay7, CCMessage::TimeCodeBBTDisplay8,
-		CCMessage::TimeCodeBBTDisplay9, CCMessage::TimeCodeBBTDisplay10,
-		CCMessage::Assignment7SegmentDisplay1, CCMessage::Assignment7SegmentDisplay2,
-		CCMessage::Assignment7SegmentDisplay3
-		});
+	constexpr bool isValidCCMessage(CCMessage mes);
 	/**
 	 * Check if the message is valid.
 	 */
-	constexpr bool isValidCCMessage(CCMessage mes) {
-		return std::find(validCCMessage.begin(), validCCMessage.end(), mes) != validCCMessage.end(); }
-	/**
-	 * Check if the message is valid.
-	 */
-	constexpr bool isValidCCMessage(int mes) {
-		return isValidCCMessage(static_cast<CCMessage>(mes)); }
+	constexpr bool isValidCCMessage(int mes);
 
 	/**
 	 * Rotation direction of wheel messages.
